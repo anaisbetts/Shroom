a6ed7d2f530851235cf4403a258b80f1e3ef0a1e
Paul Betts <paul@paulbetts.org>
2014-08-01 20:44:35 +0200

    File => New Project

---
+++ app/src/main/java/org/paulbetts/shroom/CategoryDetailFragment.java
@@ -0,0 +1,62 @@
+package org.paulbetts.shroom;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import org.paulbetts.shroom.dummy.DummyContent;
+
+/**
+ * A fragment representing a single Category detail screen.
+ * This fragment is either contained in a {@link CategoryListActivity}
+ * in two-pane mode (on tablets) or a {@link CategoryDetailActivity}
+ * on handsets.
+ */
+public class CategoryDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public CategoryDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_category_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.category_detail)).setText(mItem.content);
+        }
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/org/paulbetts/shroom/CategoryListActivity.java
@@ -0,0 +1,83 @@
+package org.paulbetts.shroom;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+
+
+
+
+/**
+ * An activity representing a list of Categories. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link CategoryDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ * <p>
+ * The activity makes heavy use of fragments. The list of items is a
+ * {@link CategoryListFragment} and the item details
+ * (if present) is a {@link CategoryDetailFragment}.
+ * <p>
+ * This activity also implements the required
+ * {@link CategoryListFragment.Callbacks} interface
+ * to listen for item selections.
+ */
+public class CategoryListActivity extends Activity
+        implements CategoryListFragment.Callbacks {
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_list);
+
+        if (findViewById(R.id.category_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-large and
+            // res/values-sw600dp). If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+
+            // In two-pane mode, list items should be given the
+            // 'activated' state when touched.
+            ((CategoryListFragment) getFragmentManager()
+                    .findFragmentById(R.id.category_list))
+                    .setActivateOnItemClick(true);
+        }
+
+        // TODO: If exposing deep links into your app, handle intents here.
+    }
+
+    /**
+     * Callback method from {@link CategoryListFragment.Callbacks}
+     * indicating that the item with the given ID was selected.
+     */
+    @Override
+    public void onItemSelected(String id) {
+        if (mTwoPane) {
+            // In two-pane mode, show the detail view in this activity by
+            // adding or replacing the detail fragment using a
+            // fragment transaction.
+            Bundle arguments = new Bundle();
+            arguments.putString(CategoryDetailFragment.ARG_ITEM_ID, id);
+            CategoryDetailFragment fragment = new CategoryDetailFragment();
+            fragment.setArguments(arguments);
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.category_detail_container, fragment)
+                    .commit();
+
+        } else {
+            // In single-pane mode, simply start the detail activity
+            // for the selected item ID.
+            Intent detailIntent = new Intent(this, CategoryDetailActivity.class);
+            detailIntent.putExtra(CategoryDetailFragment.ARG_ITEM_ID, id);
+            startActivity(detailIntent);
+        }
+    }
+}
