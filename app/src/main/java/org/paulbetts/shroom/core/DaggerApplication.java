e2b0808b848b1cb0344fd824c064fd888fffc596
Paul Betts <paul@paulbetts.org>
2014-08-03 18:42:33 +0200

    Set up Dagger

---
+++ app/src/main/java/org/paulbetts/shroom/core/DaggerApplication.java
@@ -0,0 +1,22 @@
+package org.paulbetts.shroom.core;
+
+import android.app.Application;
+
+import dagger.ObjectGraph;
+
+/**
+ * Created by paul on 8/3/14.
+ */
+public class DaggerApplication extends Application {
+    private ObjectGraph graph;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        graph = ObjectGraph.create();
+    }
+
+    public void inject(Object target){
+        graph.inject(target);
+    }
+}
---
+++ app/src/main/java/org/paulbetts/shroom/core/ForApplication.java
@@ -0,0 +1,13 @@
+package org.paulbetts.shroom.core;
+
+// Cribbed from https://github.com/square/dagger/blob/master/examples/android-simple/src/main/java/com/example/dagger/simple/ForApplication.java
+
+import java.lang.annotation.Retention;
+import javax.inject.Qualifier;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Qualifier @Retention(RUNTIME)
+public @interface ForApplication {
+}
+
