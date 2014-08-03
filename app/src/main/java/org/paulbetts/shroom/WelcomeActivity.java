855c8c61c6b340fb03d1d9016c3fcc86e42cd275
Paul Betts <paul@paulbetts.org>
2014-08-03 23:36:13 +0200

    Pick a folder, any folder!

--- app/src/main/java/org/paulbetts/shroom/WelcomeActivity.java
+++ app/src/main/java/org/paulbetts/shroom/WelcomeActivity.java
@@ -1,7 +1,15 @@
 package org.paulbetts.shroom;
 
+import android.app.Activity;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveFolder;
+import com.google.android.gms.drive.DriveId;
+import com.google.android.gms.drive.OpenFileActivityBuilder;
 
 public class WelcomeActivity extends DriveBaseActivity {
     @Override
@@ -11,12 +19,28 @@ public class WelcomeActivity extends DriveBaseActivity {
 
         this.setResult(RESULT_CANCELED);
 
-        /*
-        Button doIt = (Button)findViewById(R.id.doIt);
-        doIt.setOnClickListener(view -> {
-            WelcomeActivity.this.setResult(RESULT_OK);
-            WelcomeActivity.this.finish();
+        Button chooseFolder = (Button)findViewById(R.id.chooseFolderInDrive);
+        chooseFolder.setClickable(false);
+
+        getConnectedToDrive().subscribe(conn -> chooseFolder.setClickable(true));
+
+        chooseFolder.setOnClickListener(view -> {
+            IntentSender sender = Drive.DriveApi
+                    .newOpenFileActivityBuilder()
+                    .setMimeType(new String[] {DriveFolder.MIME_TYPE })
+                    .build(googleApiClient);
+
+            this.startObsIntentSenderForResult(sender)
+                    .filter(x -> x.getValue0() == Activity.RESULT_OK)
+                    .subscribe(x -> {
+                        DriveId driveId = x.getValue1().getParcelableExtra(
+                                OpenFileActivityBuilder.EXTRA_RESPONSE_DRIVE_ID);
+
+                        Log.i("WelcomeActivity", "Selected folder's ID: " + driveId);
+
+                        WelcomeActivity.this.setResult(RESULT_OK);
+                        WelcomeActivity.this.finish();
+                    });
         });
-        */
     }
 }
