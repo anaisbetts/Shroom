b205d6cf95b0f1d4d99bbd006aea777473cded79
Paul Betts <paul@paulbetts.org>
2014-08-05 22:13:31 +0200

    Add some more properties, protected everything

--- app/src/main/java/org/paulbetts/shroom/core/CategoryBase.java
+++ app/src/main/java/org/paulbetts/shroom/core/CategoryBase.java
@@ -1,17 +1,24 @@
 package org.paulbetts.shroom.core;
 
+import android.content.Intent;
+
 import com.google.android.gms.drive.DriveFile;
+import com.google.android.gms.drive.query.Query;
 
 /**
  * Created by paul on 8/5/14.
  */
 public abstract class CategoryBase {
-
-    private int iconId;
+    protected int iconId;
     public int getIconId() {
         return iconId;
     }
 
-    public abstract boolean interestedInFile(DriveFile file);
+    protected String categoryTitle;
+    public String getCategoryTitle() {
+        return categoryTitle;
+    }
+
+    public abstract Query generateQueryForCategory();
     public abstract Iterable<PlayableRom> loadRomsForCategory(AppSettings settings);
 }
