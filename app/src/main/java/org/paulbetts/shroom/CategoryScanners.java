8e8722bbd34d0208ffde99ef14d95438e767dcff
Paul Betts <paul@paulbetts.org>
2014-08-13 01:07:49 +0200

    Come up with a new algorithm for finding ROMs
    1. Scan for files matching the extension
    2. Get the set of folder IDs that match this extension
    3. Get the Info for those folders

--- app/src/main/java/org/paulbetts/shroom/CategoryScanners.java
+++ app/src/main/java/org/paulbetts/shroom/CategoryScanners.java
@@ -1,9 +1,27 @@
 package org.paulbetts.shroom;
 
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveId;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ListMultimap;
+import com.google.common.collect.Multimap;
+
+import org.paulbetts.shroom.core.OAuthTokenMixin;
+import org.paulbetts.shroom.gdrive.DriveItem;
+import org.paulbetts.shroom.gdrive.FileList;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+
+import rx.Observable;
+import rx.functions.Func2;
 
 /**
  * Created by paul on 8/9/14.
@@ -13,11 +31,37 @@ public class CategoryScanners {
     CategoryScanner snesScanner;
 
     @Inject
+    OAuthTokenMixin oAuthToken;
+
+    @Inject
     public CategoryScanners(SnesCategoryScanner snes) {
         snesScanner = snes;
     }
 
     public Iterable<CategoryScanner> get() {
         return Arrays.asList(new CategoryScanner[]{snesScanner,});
+    }
+
+    public Observable<List<DriveItem>> performFullScan() {
+        Observable<FileList> scans = Observable.merge(
+                Observable.from(get())
+                        .map(x -> oAuthToken.driveService.search(x.generateQueryForCategory())),
+                4);
+
+        Multimap<String, DriveItem> map = ArrayListMultimap.create();
+        Observable<Set<String>> folders = scans.reduce(map, (acc, x) -> {
+            for (DriveItem current : x.getItems()) {
+                acc.put(current.getParents().get(0).getId(), current);
+            }
+
+            return acc;
+        }).map(x -> x.keySet());
+
+        return Observable.merge(
+                folders.flatMap(x -> Observable.from(x))
+                        .map(x -> oAuthToken.driveService.getItem(x)),
+                4)
+                .toList();
+
     }
 }
