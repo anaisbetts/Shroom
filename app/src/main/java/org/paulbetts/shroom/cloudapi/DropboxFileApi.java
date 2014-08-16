b9266388d2eb5ad81eb8a82c6203c9b75b5cdaa1
Paul Betts <paul@paulbetts.org>
2014-08-16 11:18:45 -0700

    Fix our laughably incorrect code

--- app/src/main/java/org/paulbetts/shroom/cloudapi/DropboxFileApi.java
+++ app/src/main/java/org/paulbetts/shroom/cloudapi/DropboxFileApi.java
@@ -1,12 +1,15 @@
 package org.paulbetts.shroom.cloudapi;
 
+import android.os.AsyncTask;
 import android.util.Log;
 
 import com.dropbox.client2.DropboxAPI;
 import com.dropbox.client2.android.AndroidAuthSession;
+import com.dropbox.client2.exception.DropboxException;
 import com.dropbox.client2.session.AppKeyPair;
 import com.dropbox.client2.session.Session;
 
+import org.paulbetts.shroom.R;
 import org.paulbetts.shroom.core.Lifecycle;
 import org.paulbetts.shroom.core.LifecycleEvents;
 import org.paulbetts.shroom.core.RxDaggerActivity;
@@ -17,6 +20,7 @@ import rx.Observable;
 import rx.Observer;
 import rx.Subscription;
 import rx.subscriptions.CompositeSubscription;
+import rx.subscriptions.Subscriptions;
 
 /**
  * Created by paul on 8/14/14.
@@ -24,6 +28,11 @@ import rx.subscriptions.CompositeSubscription;
 public class DropboxFileApi implements CloudFileApi {
     private String token;
     private DropboxAPI<AndroidAuthSession> dropboxApi;
+    private AppKeyPair keyPair = null;
+
+    @Inject
+    public DropboxFileApi() {
+    }
 
     @Inject
     RxDaggerActivity hostActivity;
@@ -32,17 +41,18 @@ public class DropboxFileApi implements CloudFileApi {
     public String getToken() { return token; }
 
     @Override
-    public void setToken(String value) { token = value; }
+    public void setToken(String value) {
+        token = value;
+        initializeDropboxApi();
+    }
 
     @Override
     public Observable<String> authenticate() {
         return Observable.create(new Observable.OnSubscribeFunc<String>() {
             @Override
             public Subscription onSubscribe(Observer<? super String> op) {
-                AppKeyPair pair = new AppKeyPair("ou3actdv23d43xx", "hsytki045vo14ta");
-                AndroidAuthSession session = new AndroidAuthSession(pair, Session.AccessType.AUTO);
+                initializeDropboxApi();
 
-                dropboxApi = new DropboxAPI<AndroidAuthSession>(session);
                 Subscription ret = Lifecycle.getLifecycleFor(hostActivity, LifecycleEvents.RESUME)
                         .skip(1).take(1)
                         .flatMap(x -> {
@@ -57,7 +67,11 @@ public class DropboxFileApi implements CloudFileApi {
                         })
                         .subscribe(op);
 
-                dropboxApi.getSession().startOAuth2Authentication(hostActivity);
+                try {
+                    dropboxApi.getSession().startOAuth2Authentication(hostActivity);
+                } catch (Exception ex) {
+                    op.onError(ex);
+                }
                 return ret;
             }
         });
@@ -65,8 +79,46 @@ public class DropboxFileApi implements CloudFileApi {
 
     @Override
     public Observable<Boolean> testToken(String token) {
-        return dropboxApi.getSession().authenticationSuccessful() ?
-                Observable.just(true) :
-                Observable.error(new Exception("Token is not valid"));
+        if (dropboxApi == null) initializeDropboxApi();
+
+        return Observable.create(new Observable.OnSubscribeFunc<Boolean>() {
+            @Override
+            public Subscription onSubscribe(Observer<? super Boolean> op) {
+                AsyncTask t = new AsyncTask() {
+                    @Override
+                    protected Void doInBackground(Object[] objects) {
+                        try {
+                            DropboxAPI.Account ac = dropboxApi.accountInfo();
+                            Log.i("DropboxFileApi", ac.displayName);
+
+                            op.onNext(true);
+                            op.onCompleted();
+                        } catch (DropboxException de) {
+                            op.onNext(false);
+                            op.onCompleted();
+                        }
+
+                        return null;
+                    }
+                };
+
+                t.execute();
+                return Subscriptions.create(() -> t.cancel(false));
+            }
+        });
     }
+
+    private void initializeDropboxApi() {
+        keyPair = keyPair != null ? keyPair : new AppKeyPair(
+                hostActivity.getString(R.string.dropbox_key),
+                hostActivity.getString(R.string.dropbox_secret));
+
+        AndroidAuthSession session = new AndroidAuthSession(keyPair);
+        dropboxApi = new DropboxAPI(session);
+
+        if (token != null) {
+            dropboxApi.getSession().setOAuth2AccessToken(token);
+        }
+    }
+
 }
