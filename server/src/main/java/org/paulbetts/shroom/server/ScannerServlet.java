bb72689e6f18c653ee1558a4b3003d7d9d0628e0
Paul Betts <paul@paulbetts.org>
2014-08-22 21:31:42 -0700

    AppEngine apparently hates concurrency

--- server/src/main/java/org/paulbetts/shroom/server/ScannerServlet.java
+++ server/src/main/java/org/paulbetts/shroom/server/ScannerServlet.java
@@ -6,6 +6,9 @@ import com.dropbox.core.DbxEntry;
 import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
 
+import com.google.appengine.api.memcache.Expiration;
+import com.google.appengine.api.memcache.MemcacheService;
+import com.google.appengine.api.memcache.MemcacheServiceFactory;
 import com.google.appengine.repackaged.org.codehaus.jackson.map.ObjectMapper;
 
 import java.io.IOException;
@@ -15,6 +18,7 @@ import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -22,20 +26,34 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import rx.Observable;
+import rx.Observer;
+import rx.Subscription;
 import rx.functions.Action1;
 import rx.functions.Func0;
 import rx.functions.Func1;
-import rx.functions.Func2;
-import rx.util.async.Async;
+import rx.subscriptions.Subscriptions;
 
 public class ScannerServlet extends HttpServlet {
     static ObjectMapper om = new ObjectMapper();
+    private static final Logger log = Logger.getLogger(ScannerServlet.class.getName());
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String token = req.getParameter("dropbox_token");
         if (token == null) {
             failRequestWith(400, "Bad Token", resp);
+            return;
+        }
+
+        String memcacheKey = "scanner_" + token;
+        MemcacheService memcache = MemcacheServiceFactory.getMemcacheService();
+        Object cachedResponse = memcache.get(memcacheKey);
+
+        if (cachedResponse != null) {
+            resp.setContentType("application/json");
+            PrintWriter w = resp.getWriter();
+            w.write((String) cachedResponse);
+            w.close();
             return;
         }
 
@@ -48,6 +66,7 @@ public class ScannerServlet extends HttpServlet {
                 .publish()
                 .refCount();
 
+        final ArrayList<String> lines = new ArrayList<>();
         scans.subscribe(new Action1<List<DbxEntry>>() {
             @Override
             public void call(List<DbxEntry> dbxEntries) {
@@ -55,7 +74,9 @@ public class ScannerServlet extends HttpServlet {
                     try {
                         StringWriter sw = new StringWriter();
                         om.writeValue(sw, e);
-                        writer.write(sw.toString().replace('\n', ' ') + "\n");
+                        String line = sw.toString().replace('\n', ' ') + "\n";
+                        writer.write(line);
+                        lines.add(line);
                     } catch (IOException ex) {
                         ex.printStackTrace();
                     }
@@ -64,9 +85,41 @@ public class ScannerServlet extends HttpServlet {
         });
 
         scans.toBlocking().last();
+
+        memcache.put(memcacheKey, join("", lines),
+                Expiration.byDeltaSeconds(60), MemcacheService.SetPolicy.SET_ALWAYS);
         writer.close();
     }
 
+    private Observable<List<DbxEntry>> searchForRoms(final DbxClient client, final List<String> extensions) {
+        return Observable.create(new Observable.OnSubscribeFunc<List<DbxEntry>>() {
+            @Override
+            public Subscription onSubscribe(Observer<? super List<DbxEntry>> op) {
+                try {
+                    for(String ext: extensions) {
+                        op.onNext(client.searchFileAndFolderNames("/", ext));
+                    }
+                } catch (DbxException e) {
+                    op.onError(e);
+                }
+
+                op.onCompleted();
+                return Subscriptions.empty();
+            }
+        });
+    }
+
+    String join(String delimiter, List<String> lines) {
+        StringBuilder sb = new StringBuilder();
+        for(String line: lines) {
+            sb.append(line);
+            sb.append(delimiter);
+        }
+
+        return sb.toString();
+    }
+
+    /*
     private Observable<List<DbxEntry>> searchForRoms(final DbxClient client, List<String> extensions) {
         Observable<Observable<List<DbxEntry>>> scans = Observable.from(extensions)
                 .map(new Func1<String, Observable<List<DbxEntry>>>() {
@@ -76,7 +129,11 @@ public class ScannerServlet extends HttpServlet {
                             @Override
                             public List<DbxEntry> call() {
                                 try {
-                                    return client.searchFileAndFolderNames("/", s);
+                                    log.warning("Attempting search!");
+                                    List<DbxEntry> ret = client.searchFileAndFolderNames("/", s);
+
+                                    log.warning("Finished search!");
+                                    return ret;
                                 } catch (DbxException e) {
                                     throw new RuntimeException(e);
                                 }
@@ -87,6 +144,7 @@ public class ScannerServlet extends HttpServlet {
 
         return Observable.merge(scans, 8);
     }
+    */
 
     private void failRequestWith(int code, String message, HttpServletResponse resp) throws IOException {
         Writer writer = resp.getWriter();
