550e7500bd5d586df7d3ff3475a5b71961ad56f6
Paul Betts <paul@paulbetts.org>
2014-08-22 19:54:58 -0700

    Write the most basic code possible to scan a Dropbox

--- server/src/main/java/org/paulbetts/shroom/server/ScannerServlet.java
+++ server/src/main/java/org/paulbetts/shroom/server/ScannerServlet.java
@@ -2,25 +2,32 @@ package org.paulbetts.shroom.server;
 
 import com.dropbox.core.DbxAccountInfo;
 import com.dropbox.core.DbxClient;
+import com.dropbox.core.DbxEntry;
 import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.google.api.server.spi.config.Api;
+
 import com.google.appengine.repackaged.org.codehaus.jackson.map.ObjectMapper;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Created by paul on 8/21/14.
- */
+import rx.Observable;
+import rx.functions.Action1;
+import rx.functions.Func0;
+import rx.functions.Func1;
+import rx.functions.Func2;
+import rx.util.async.Async;
+
 public class ScannerServlet extends HttpServlet {
     static ObjectMapper om = new ObjectMapper();
 
@@ -34,18 +41,51 @@ public class ScannerServlet extends HttpServlet {
 
         DbxClient client = new DbxClient(new DbxRequestConfig("ShroomService", "en", new AppengineHttpRequestor()), token);
 
-        try {
-            DbxAccountInfo accountInfo = client.getAccountInfo();
+        resp.setContentType("application/json");
+        final PrintWriter writer = resp.getWriter();
 
-            resp.setContentType("application/json");
+        Observable<List<DbxEntry>> scans = searchForRoms(client, Arrays.asList("smc"))
+                .publish()
+                .refCount();
 
-            PrintWriter writer = resp.getWriter();
-            om.writeValue(writer, accountInfo);
-        } catch (DbxException e) {
-            e.printStackTrace();
-            failRequestWith(500, e.getMessage(), resp);
-            return;
-        }
+        scans.subscribe(new Action1<List<DbxEntry>>() {
+            @Override
+            public void call(List<DbxEntry> dbxEntries) {
+                for(DbxEntry e: dbxEntries) {
+                    try {
+                        StringWriter sw = new StringWriter();
+                        om.writeValue(sw, e);
+                        writer.write(sw.toString().replace('\n', ' ') + "\n");
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        scans.toBlocking().last();
+        writer.close();
+    }
+
+    private Observable<List<DbxEntry>> searchForRoms(final DbxClient client, List<String> extensions) {
+        Observable<Observable<List<DbxEntry>>> scans = Observable.from(extensions)
+                .map(new Func1<String, Observable<List<DbxEntry>>>() {
+                    @Override
+                    public Observable<List<DbxEntry>> call(final String s) {
+                        return RxAppEngine.deferStart(new Func0<List<DbxEntry>>() {
+                            @Override
+                            public List<DbxEntry> call() {
+                                try {
+                                    return client.searchFileAndFolderNames("/", s);
+                                } catch (DbxException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            }
+                        });
+                    }
+                });
+
+        return Observable.merge(scans, 8);
     }
 
     private void failRequestWith(int code, String message, HttpServletResponse resp) throws IOException {
