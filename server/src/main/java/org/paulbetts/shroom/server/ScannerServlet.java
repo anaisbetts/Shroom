0e31b5e513e8ac0dbbda4d0650077bc71025e3d9
Paul Betts <paul@paulbetts.org>
2014-08-23 18:14:57 -0700

    Return a RomInfo object instead of a DbxEntry

--- server/src/main/java/org/paulbetts/shroom/server/ScannerServlet.java
+++ server/src/main/java/org/paulbetts/shroom/server/ScannerServlet.java
@@ -10,10 +10,13 @@ import com.google.appengine.api.memcache.MemcacheService;
 import com.google.appengine.api.memcache.MemcacheServiceFactory;
 import com.google.appengine.repackaged.org.codehaus.jackson.map.ObjectMapper;
 
+import org.paulbetts.shroom.models.RomInfo;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -26,6 +29,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import rx.Observable;
 import rx.Observer;
+import rx.Subscriber;
 import rx.subscriptions.Subscriptions;
 
 public class ScannerServlet extends HttpServlet {
@@ -57,13 +61,13 @@ public class ScannerServlet extends HttpServlet {
         resp.setContentType("application/json");
         final PrintWriter writer = resp.getWriter();
 
-        Observable<List<DbxEntry>> scans = searchForRoms(client, Arrays.asList("smc"))
+        Observable<List<RomInfo>> scans = searchForRoms(client, Arrays.asList("smc"))
                 .publish()
                 .refCount();
 
         final ArrayList<String> lines = new ArrayList<>();
-        scans.subscribe(dbxEntries -> {
-            for(DbxEntry e: dbxEntries) {
+        scans.subscribe(roms -> {
+            for(RomInfo e: roms) {
                 try {
                     StringWriter sw = new StringWriter();
                     om.writeValue(sw, e);
@@ -83,18 +87,28 @@ public class ScannerServlet extends HttpServlet {
         writer.close();
     }
 
-    private Observable<List<DbxEntry>> searchForRoms(final DbxClient client, final List<String> extensions) {
-        return Observable.create((Observer<? super List<DbxEntry>> op) -> {
+    private Observable<List<RomInfo>> searchForRoms(final DbxClient client, final List<String> extensions) {
+        return Observable.create((Subscriber<? super List<RomInfo>> op) -> {
             try {
                 for (String ext : extensions) {
-                    op.onNext(client.searchFileAndFolderNames("/", ext));
+                    if (op.isUnsubscribed()) break;
+
+                    ArrayList<RomInfo> ret = new ArrayList<>();
+                    for(DbxEntry e: client.searchFileAndFolderNames("/", ext)) {
+                        // NB: There's no reason that we won't get unrelated files
+                        // that happen to have 'smc' et al in the name as a token.
+                        if (!e.name.endsWith(ext) || !e.isFile()) return;
+
+                        ret.add(dropboxToRomInfo(e));
+                    }
+
+                    op.onNext(ret);
                 }
             } catch (DbxException e) {
                 op.onError(e);
             }
 
             op.onCompleted();
-            return Subscriptions.empty();
         });
     }
 
@@ -134,6 +148,10 @@ public class ScannerServlet extends HttpServlet {
         }
 
         public String message;
+    }
+
+    private RomInfo dropboxToRomInfo(DbxEntry entry) {
+        return new RomInfo(entry.name, "", "", entry.path);
     }
 
     String joinStrings(String delimiter, List<String> lines) {
