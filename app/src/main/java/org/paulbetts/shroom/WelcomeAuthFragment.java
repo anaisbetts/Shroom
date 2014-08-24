d688a7962c39ab46337837808fc26c3905b90b91
Paul Betts <paul@paulbetts.org>
2014-08-23 22:07:09 -0700

    Finish up the welcome flow
    We don’t actually save any of the ROMs we’ve found yet, but we’ve got
    all of the infrastructure to do so

--- app/src/main/java/org/paulbetts/shroom/WelcomeAuthFragment.java
+++ app/src/main/java/org/paulbetts/shroom/WelcomeAuthFragment.java
@@ -1,15 +1,20 @@
 package org.paulbetts.shroom;
 
 import android.os.Bundle;
+import android.transition.TransitionManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
+import org.paulbetts.shroom.cloudapi.CloudFileApi;
 import org.paulbetts.shroom.core.OAuthTokenMixin;
 import org.paulbetts.shroom.core.RxDaggerActivity;
 import org.paulbetts.shroom.core.RxDaggerFragment;
+import org.paulbetts.shroom.models.RomInfo;
 
 import javax.inject.Inject;
 
@@ -17,10 +22,14 @@ import butterknife.ButterKnife;
 import butterknife.InjectView;
 import rx.Observable;
 import rx.android.observables.ViewObservable;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.subjects.PublishSubject;
 
 public class WelcomeAuthFragment extends RxDaggerFragment {
     @InjectView(R.id.auth_dropbox) Button authDropbox = null;
+    @InjectView(R.id.search_scene) LinearLayout searchScene = null;
+    @InjectView(R.id.progress) ProgressBar progress = null;
+    @InjectView(R.id.finish_login) Button finishLogin = null;
 
     @Inject
     RxDaggerActivity hostActivity;
@@ -28,19 +37,46 @@ public class WelcomeAuthFragment extends RxDaggerFragment {
     @Inject
     OAuthTokenMixin oAuthTokenMixin;
 
+    @Inject
+    CloudFileApi dropboxApi;
+
     public WelcomeAuthFragment() {
         // Required empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View ret = inflater.inflate(R.layout.fragment_welcome_auth, container, false);
+        final View ret = inflater.inflate(R.layout.fragment_welcome_auth, container, false);
         ButterKnife.inject(this, ret);
 
-        ViewObservable.clicks(authDropbox, false)
+        Observable<RomInfo> roms = ViewObservable.clicks(authDropbox, false).take(1)
                 .doOnNext(x -> oAuthTokenMixin.forgetExistingToken())
                 .flatMap(x -> oAuthTokenMixin.initializeHelper(this))
                 .doOnNext(x -> Log.i("Shroom", "Logged into Cloud File Backend successfully"))
+                .flatMap(x -> {
+                    TransitionManager.beginDelayedTransition(container);
+                    authDropbox.setVisibility(View.GONE);
+                    searchScene.setVisibility(View.VISIBLE);
+
+                    return dropboxApi.scanForRoms();
+                })
+                .doOnNext(x -> {
+                    Log.i("WelcomeActivity", x.getTitle());
+                })
+                .publish()
+                .refCount();
+
+        roms
+                .reduce(true, (acc,x) -> acc)
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnNext(x -> {
+                    TransitionManager.beginDelayedTransition(container);
+                    progress.setVisibility(View.GONE);
+                })
+                .flatMap(x -> {
+                    return ViewObservable.clicks(finishLogin, false).take(1)
+                            .map(y -> true);
+                })
                 .multicast(loginComplete)
                 .connect();
 
